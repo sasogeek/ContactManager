69068736119a26d63a2ef74ead1986b739d62fab
Samuel Sowah <samuel@official-notebook.com>
2017-09-18 10:21:57 +0000

    modified contact.py

--- contact.py
+++ contact.py
@@ -8,8 +8,9 @@ class Contact:
         self.birthday = input('Birthday: ')
         self.linked_in = input('Linked In: ')
         # self.picture = input('Picture: ')
-        self.contact_[self.name] = {'phone number': self.phone_number,
-                                    'email': self.email,
-                                    'website': self.website,
-                                    'birthday': self.birthday,
-                                    'linkedin': self.linked_in}
+        self.contact_ = {'name': self.name,
+                         'phone number': self.phone_number,
+                         'email': self.email,
+                         'website': self.website,
+                         'birthday': self.birthday,
+                         'linkedin': self.linked_in}
