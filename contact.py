7ce4fa3bddb1c2e414e97cb3fb8a48b87af55b7b
Samuel Sowah <samuel@official-notebook.com>
2017-09-18 10:03:48 +0000

    initial commit

---
+++ contact.py
@@ -0,0 +1,15 @@
+class Contact:
+    def __init__(self):
+        self.contact_ = {}
+        self.name = input('Name: ')
+        self.phone_number = input('Phone Number: ')
+        self.email = input('Email: ')
+        self.website = input('Website: ')
+        self.birthday = input('Birthday: ')
+        self.linked_in = input('Linked In: ')
+        # self.picture = input('Picture: ')
+        self.contact_[self.name] = {'phone number': self.phone_number,
+                                    'email': self.email,
+                                    'website': self.website,
+                                    'birthday': self.birthday,
+                                    'linkedin': self.linked_in}
